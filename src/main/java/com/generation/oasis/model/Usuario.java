25a3a35f3e4afa0f616706585a780a12cacb3127
guilhermerubrae <guilhermerubrae@gmail.com>
2024-01-19 15:26:21 -0300

    get e set e relacionamento

--- src/main/java/com/generation/oasis/model/Usuario.java
+++ src/main/java/com/generation/oasis/model/Usuario.java
@@ -31,4 +31,44 @@ public class Usuario {
 
     @NotNull
     private String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
