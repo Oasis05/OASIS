65184e7b7fc91e256a01ac37784670005e1dc6e0
guilhermerubrae <guilhermerubrae@gmail.com>
2024-01-19 15:10:33 -0300

    produto

---
+++ src/main/java/com/generation/oasis/model/Produto.java
@@ -0,0 +1,81 @@
+package com.generation.oasis.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo nome é obrigatório!")
+    @Size(min = 5, max = 255, message = "O atributo nome deve conter no mínimo 5 e no máximo 255 caracteres")
+    private String nome;
+
+    @NotNull(message = "O atributo data_validade é obrigatório!")
+    private Date dataValidade;
+
+    @NotNull(message = "O atributo quantidade é obrigatório!")
+    private Integer quantidade;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @NotNull(message = "Preço é obrigatório!")
+    @Positive(message = "O preço deve ser maior do que zero!")
+    private BigDecimal preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(Date dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+    
+    
+}
